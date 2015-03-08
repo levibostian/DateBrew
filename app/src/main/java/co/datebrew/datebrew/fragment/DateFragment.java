b9aeae3aa2646f42f0e37f6b76f8c6314ef643c5
Levi Bostian <levi.bostian>
2015-03-08 12:57:31 -0500

    Add view details for date card.

--- app/src/main/java/co/datebrew/datebrew/fragment/DateFragment.java
+++ app/src/main/java/co/datebrew/datebrew/fragment/DateFragment.java
@@ -6,9 +6,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import co.datebrew.datebrew.R;
 
 public class DateFragment extends Fragment {
+
+    private ImageView mDateImageView;
+    private static final int DATE_BACK = R.drawable.date_card_back;
+    private static final int DATE_FRONT = R.drawable.date_card;
+    private boolean mIsDateCardFront = true;
 
     public static DateFragment newInstance() {
         return new DateFragment();
@@ -17,7 +23,22 @@ public class DateFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_date, container, false);
+        View view = inflater.inflate(R.layout.fragment_date, container, false);
+
+        mDateImageView = (ImageView) view.findViewById(R.id.date_imageview);
+        mDateImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleDateDetails();
+            }
+        });
+
+        return view;
+    }
+
+    private void toggleDateDetails() {
+        mDateImageView.setImageResource(mIsDateCardFront ? DATE_BACK : DATE_FRONT);
+        mIsDateCardFront = !mIsDateCardFront;
     }
 
 }
